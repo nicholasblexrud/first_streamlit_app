63ea232d4bbfd01933949c7745108428264bc79e
Nick B <nick@blexrud.com>
2022-08-27 16:49:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -8,4 +9,7 @@ streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
