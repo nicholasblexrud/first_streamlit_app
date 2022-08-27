5ef3db3b6ccc006bcf5b81bb88d99145e461769a
Nick B <nick@blexrud.com>
2022-08-27 16:46:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-🥣 🥗 🐔 🥑🍞
+
 streamlit.header('🥣Breakfast Menu')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
