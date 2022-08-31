0f4d5dbc4d656add64ffb331d0e04886aee7ea4a
Nick B <nick@blexrud.com>
2022-08-30 21:24:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas
-import request
+import requests
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
