d26709bdbde71ca5693114ad4d3663d054f142f8
Nick B <nick@blexrud.com>
2022-08-30 21:25:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import pandas
 import requests
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -24,4 +23,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header("Fruityvice Fruit Advice!")
+streamlit.text(fruityvice_response)
+
 
