d87591c9f020db349b1b024c0fd2c956e0c7d754
Nick B <nick@blexrud.com>
2022-08-30 21:26:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 
